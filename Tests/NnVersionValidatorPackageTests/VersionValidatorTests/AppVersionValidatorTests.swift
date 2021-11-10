eb65c07dc871d31d75f527a4b169a8b42e143104
Nikolai Nobadi <nikolai.rivas@gmail.com>
2021-11-10 13:30:10 -0800

    Added test file for AppVersionValidator

---
+++ Tests/NnVersionValidatorPackageTests/VersionValidatorTests/AppVersionValidatorTests.swift
@@ -0,0 +1,22 @@
+//
+//  AppVersionValidatorTests.swift
+//  
+//
+//  Created by Nikolai Nobadi on 11/10/21.
+//
+
+import XCTest
+
+class AppVersionValidatorTests: XCTestCase {
+    
+    
+}
+
+
+// MARK: - SUT
+extension AppVersionValidatorTests {
+    
+    func makeSUT() {
+        
+    }
+}
