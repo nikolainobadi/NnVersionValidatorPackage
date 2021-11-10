b1e5842efac6906e67c0463a06453b9eb1161972
Nikolai Nobadi <nikolai.rivas@gmail.com>
2021-11-10 10:22:10 -0800

    Initial Commit

---
+++ Tests/NnVersionValidatorPackageTests/AppVersionValidatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  AppVersionValidatorTests.swift
+//  
+//
+//  Created by Nikolai Nobadi on 11/10/21.
+//
+
+import XCTest
+
+class AppVersionValidator {
+    
+    // MARK: - Properties
+    let versionURL: URL
+    
+    
+    // MARK: - Init
+    init(versionURL: URL) {
+        self.versionURL = versionURL
+    }
+}
+
+class AppVersionValidatorTests: XCTestCase {
+    
+    
+}
+
+
+// MARK: - SUT
+extension AppVersionValidatorTests {
+    
+    func makeSUT() {
+        
+    }
+}
