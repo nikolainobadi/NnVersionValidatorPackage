ff7f37a43e1aeef118eab022ce454758e1a94cc9
Nikolai Nobadi <nikolai.rivas@gmail.com>
2021-11-10 11:07:43 -0800

    Moved HTTPClientSpy to its own file

---
+++ Tests/NnVersionValidatorPackageTests/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,61 @@
+//
+//  HTTPClientSpy.swift
+//  
+//
+//  Created by Nikolai Nobadi on 11/10/21.
+//
+
+import XCTest
+import NnVersionValidatorPackage
+
+class HTTPClientSpy: HTTPClient {
+    
+    private var actions = [(url: URL,
+                             completion: (HTTPClient.Result) -> Void)]()
+
+    var requestedURLs: [URL] {
+        return actions.map { $0.url }
+    }
+
+    func get(from url: URL,
+             completion: @escaping (HTTPClient.Result) -> Void) {
+        
+        actions.append((url, completion))
+    }
+
+    func complete(with error: Error,
+                  at index: Int = 0,
+                  file: StaticString = #filePath,
+                  line: UInt = #line) {
+        guard
+            actions.count > index
+        else {
+            return XCTFail("Can't complete request never made",
+                           file: file, line: line)
+        }
+
+        actions[index].completion(.failure(error))
+    }
+
+    func complete(withStatusCode code: Int,
+                  data: Data,
+                  at index: Int = 0,
+                  file: StaticString = #filePath,
+                  line: UInt = #line) {
+        guard
+            requestedURLs.count > index
+        else {
+            return XCTFail("Can't complete request never made",
+                           file: file, line: line)
+        }
+
+        let response = HTTPURLResponse(
+            url: requestedURLs[index],
+            statusCode: code,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+
+        actions[index].completion(.success((data, response)))
+    }
+}
